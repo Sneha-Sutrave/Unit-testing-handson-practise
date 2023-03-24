6ea074eec26c709a27391761d814c44bd19b5fe4
Sneha-Sutrave <61264284+Sneha-Sutrave@users.noreply.github.com>
2023-03-24 13:00:45 +0530

    Update errorAndSuccess.test.js

--- test/errorAndSuccess.test.js
+++ test/errorAndSuccess.test.js
@@ -4,9 +4,25 @@ import '../src/SuccessAndError/Success.js';
 import '../src/SuccessAndError/Error.js';
 
 describe('Success screen ', () => {
-  // Write test cases inside this block
+ it('Check for the Accessible', async () => {
+    expect(obj).to.be.accessible();
+  });
+
+  it('Spy on the _toHome ', async () => {
+    const mySpyMethod = Sinon.spy(obj, '_toHome');
+    obj._toHome();
+    expect(mySpyMethod.calledOnce).to.be.true;
+  });
 });
 
 describe('error screen', () => {
-  // Write test cases inside this block
+  it('Checks for the Accessible', async () => {
+    expect(obj2).to.be.accessible();
+  });
+  it('Spy on the _toHome', async () => {
+    const obj2 = await fixture(html`<loan-error></loan-error>`);
+    const spyMethod = Sinon.spy(obj2, '_toHome');
+    obj2._toHome();
+    expect(spyMethod.calledOnce).to.be.true;
+  });
 });

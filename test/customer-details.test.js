e752318c933e946c5fcaa2623623d267294340b6
Sneha-Sutrave <61264284+Sneha-Sutrave@users.noreply.github.com>
2023-03-24 12:50:40 +0530

    Update customer-details.test.js

--- test/customer-details.test.js
+++ test/customer-details.test.js
@@ -3,5 +3,12 @@ import { stub } from 'sinon';
 import '../src/Customer/Customer-details.js';
 
 describe('customer details', () => {
-  // Write test cases inside this block
+  it('checks for the Accessible', async () => {
+    expect(obj).to.be.accessible; 
+  });
+  it('checks for the spy to _toEmidetails', () => {
+    const SpyMethod = Sinon.spy(obj, '_toEmidetails');
+    obj._toEmidetails();
+    expect(SpyMethod.calledOnce).to.be.true;
+  });
 });
